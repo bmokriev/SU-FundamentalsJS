9fa1ddc897d45a5b811c0cdddc5569afe311807b
bmokriev <66565502+bmokriev@users.noreply.github.com>
2020-10-26 09:43:59 +0200

    Excercises

---
+++ DataTypes&Variables/charsToStr.js
@@ -0,0 +1,12 @@
+function chars(arg1, arg2 ,arg3) {
+
+    let total = ''
+    total += arg1
+    total += arg2
+    total += arg3
+    
+    console.log(total);
+}
+chars('a',
+'b',
+'c')
---
+++ DataTypes&Variables/gbpToUsd.js
@@ -0,0 +1,6 @@
+function gbpToUsd(gbp) {
+    gbp = +gbp;
+    let usd = gbp * 1.31;
+    console.log(usd.toFixed(3));
+    
+}
