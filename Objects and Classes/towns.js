8a473e1325b300e53694a1a35a2a05b90bd64f65
bmokriev <66565502+bmokriev@users.noreply.github.com>
2020-11-28 07:05:20 +0200

    ex

---
+++ Objects and Classes/towns.js
@@ -0,0 +1,16 @@
+function towns(arr) {
+
+    for (const city of arr) {
+        let [town, latitude, longitude] = city.split(' | ')
+        let obj = {
+            town,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2)
+        }
+        console.log(obj);
+    }
+
+}
+towns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']
+)
