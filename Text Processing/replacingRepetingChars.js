eee185a23d6f6b632329bdff3e89230e70b3f1aa
bmokriev <66565502+bmokriev@users.noreply.github.com>
2020-12-14 18:55:57 +0200

    ex

---
+++ Text Processing/replacingRepetingChars.js
@@ -0,0 +1,11 @@
+function replacing(str) {
+    let result = ''
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i - 1]) {
+            result += str[i]
+        }
+    }
+    console.log(result);
+
+}
+replacing('qqqwerqwecccwd')
