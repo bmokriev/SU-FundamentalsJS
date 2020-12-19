77b61c5d1be59d32752e506ed9e11cacd9f31e91
bmokriev <66565502+bmokriev@users.noreply.github.com>
2020-12-19 13:12:30 +0200

    ex

---
+++ Text Processing/hardWords.js
@@ -0,0 +1,13 @@
+function hardWords(input) {
+    let str = input[0];
+    const wordsArr = input[1]
+        .sort((a, b) => b.length - a.length)
+
+    for (const word of wordsArr) {
+        str = str.replace('_'.repeat(word.length), word)
+    }
+    console.log(str);
+}
+hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+    ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]
+)
